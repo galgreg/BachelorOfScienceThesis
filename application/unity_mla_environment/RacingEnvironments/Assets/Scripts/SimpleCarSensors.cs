cc7ba3596df995459343c965e59bf3b1955ff418
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-08-05 11:55:40 +0200

    Dostosowanie gita do poprawnej obsługi projektu w Unity

---
+++ application/unity_mla_environment/RacingEnvironments/Assets/Scripts/SimpleCarSensors.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCarSensors : MonoBehaviour
+{
+    public void GetSensorsInput() {
+        
+    }
+    private void FixedUpdate() {
+        GetSensorsInput();
+    }
+
+    public GameObject agentCar;
+    public float sensorLength = 2.0f;
+}
