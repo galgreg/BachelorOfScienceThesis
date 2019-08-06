d7c984246f74aa66c0e2de1d2c1b4d859db48a97
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-08-06 16:04:46 +0200

    WIP: Praca nad sensorami samochodu

---
+++ application/unity_mla_environment/RacingEnvironments/Assets/Scripts/CarSensor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class CarSensor {
+    public CarSensor() {
+        LAYER_MASK = 1 << LayerMask.NameToLayer("RaceTrackLayer");
+        mRayProperties = new Ray(Vector3.zero, Vector3.zero);
+        mRaycastHit = new RaycastHit();
+        mSensorRenderer = new LineRenderer();
+    }
+    public void SetRayProperties(Vector3 aOrigin, Vector3 aDirection) {
+        mRayProperties.origin = aOrigin;
+        mRayProperties.direction = aDirection;
+    }
+    
+    public float GetDistance() {
+        bool wasObstacleDetected = Physics.Raycast(
+                mRayProperties,
+                out mRaycastHit,
+                MAX_SENSOR_LENGTH,
+                LAYER_MASK);
+
+        if (wasObstacleDetected) {
+            return mRaycastHit.distance;
+        } else {
+            return MAX_SENSOR_LENGTH;
+        }
+    }
+    public void Render() {
+        // TODO
+        throw new NotImplementedException();
+    }
+
+    private readonly Color MINIMUM_LENGTH_COLOR = Color.red;
+    private readonly Color MAXIMUM_LENGTH_COLOR = Color.green;
+    private const float MAX_SENSOR_LENGTH = 0.5f;
+    private readonly int LAYER_MASK;
+    
+    private Ray mRayProperties;
+    private RaycastHit mRaycastHit;
+    private LineRenderer mSensorRenderer;
+}
---
+++ application/unity_mla_environment/RacingEnvironments/Assets/Scripts/SensorsKit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorsKit : MonoBehaviour {
+    // Initialization code
+    private void Start() {
+        InitSensorList();
+        // TODO -> czy trzeba tu coś jeszcze dopisać?
+    }
+    private void InitSensorList() {
+        const uint ANGLE_BETWEEN_SENSORS = FIELD_OF_VIEW / (RAYS_COUNT - 1);
+        for (uint i = 0; i < RAYS_COUNT; ++i) {
+            uint currentSensorAngle = ANGLE_BETWEEN_SENSORS * i;
+            var newSensor = CreateNewSensor(currentSensorAngle);
+            mSensorList.Add(newSensor);
+        }
+    }
+    private CarSensor CreateNewSensor(uint aCurrentSensorAngle) {
+        var newSensor = new CarSensor();
+        Vector3 sensorOrigin = ComputeSensorOrigin();
+        Vector3 sensorDirection = ComputeSensorDirection(aCurrentSensorAngle);
+        newSensor.SetRayProperties(sensorOrigin, sensorDirection);
+        return newSensor;
+    }
+    private Vector3 ComputeSensorOrigin() {
+        throw new NotImplementedException();
+    }
+    private Vector3 ComputeSensorDirection(uint aCurrentSensorAngle) {
+        throw new NotImplementedException();
+    }
+    private List<double> GetDistanceList() {
+        List<double> distanceList = new List<double>();
+        foreach (var sensor in mSensorList) {
+            distanceList.Add(sensor.GetDistance());
+        }
+        return distanceList;
+    }
+    public Transform mCarTransform;
+    private const uint FIELD_OF_VIEW = 180;
+    private const uint RAYS_COUNT = 3;
+    private List<CarSensor> mSensorList;
+}
