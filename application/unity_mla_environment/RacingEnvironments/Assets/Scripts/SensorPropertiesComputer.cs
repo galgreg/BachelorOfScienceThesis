34ce9be51606c43719211a123244792a631fc157
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-08-07 21:08:49 +0200

    Wydzielenie części prywatnych metod klasy SensorsKit do nowej klasy: SensorPropertiesComputer

---
+++ application/unity_mla_environment/RacingEnvironments/Assets/Scripts/SensorPropertiesComputer.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class SensorPropertiesComputer {
+    public SensorPropertiesComputer(
+            Transform aCarTransform, float aOffsetY, float aOffsetZ) {
+        CAR_TRANSFORM = aCarTransform;
+        SENSOR_OFFSET_Y = aOffsetY;
+        SENSOR_OFFSET_Z = aOffsetZ;
+    }
+    public Vector3 ComputeSensorOrigin() {
+        var carPosition = CAR_TRANSFORM.position;
+        Vector3 sensorOriginBeforeRotation =
+                new Vector3(
+                    carPosition.x,
+                    carPosition.y + SENSOR_OFFSET_Y,
+                    carPosition.z + SENSOR_OFFSET_Z);
+        float carRotationAngle = CAR_TRANSFORM.eulerAngles.y;
+        
+        Vector3 sensorOriginAfterRotation =
+                ComputePointRotation(
+                        sensorOriginBeforeRotation,
+                        carPosition,
+                        carRotationAngle);
+        return sensorOriginAfterRotation;
+    }
+    public Vector3 ComputeSensorDirection(float aCurrentSensorAngle) {
+        var carPosition = CAR_TRANSFORM.position;
+        Vector3 sensorOrigin =
+                new Vector3(
+                        carPosition.x,
+                        carPosition.y + SENSOR_OFFSET_Y,
+                        carPosition.z + SENSOR_OFFSET_Z);
+        Vector3 sensorDirectionBeforeRotation =
+                new Vector3(
+                        sensorOrigin.x - 0.5f,
+                        sensorOrigin.y,
+                        sensorOrigin.z);
+        Vector3 directionRotatedBySensorAngle = ComputePointRotation(
+                sensorDirectionBeforeRotation,
+                sensorOrigin,
+                aCurrentSensorAngle);
+        float carRotationAngle = CAR_TRANSFORM.eulerAngles.y;
+        Vector3 directionRotatedByCarAngle = ComputePointRotation(
+                directionRotatedBySensorAngle,
+                carPosition,
+                carRotationAngle);
+        return directionRotatedByCarAngle;
+    }
+    private Vector3 ComputePointRotation(
+            Vector3 pointToRotate,
+            Vector3 rotationOrigin,
+            float angleInDegrees) {
+        float angleInRadians = angleInDegrees * Mathf.PI / 180.0f;
+        float rotatedX =
+                Mathf.Cos(angleInRadians) * (pointToRotate.x - rotationOrigin.x)
+                - Mathf.Sin(angleInRadians) * (pointToRotate.z - rotationOrigin.z)
+                + rotationOrigin.x;
+        float rotatedZ =
+                Mathf.Sin(angleInRadians) * (pointToRotate.x - rotationOrigin.x)
+                + Mathf.Cos(angleInRadians) * (pointToRotate.z - rotationOrigin.z)
+                + rotationOrigin.z;
+        
+        Vector3 rotatedPoint = new Vector3(rotatedX, pointToRotate.y, rotatedZ);
+        return rotatedPoint;
+    }
+
+    private readonly Transform CAR_TRANSFORM;
+    private readonly float SENSOR_OFFSET_Y;
+    private readonly float SENSOR_OFFSET_Z;
+}
--- application/unity_mla_environment/RacingEnvironments/Assets/Scripts/SensorsKit.cs
+++ application/unity_mla_environment/RacingEnvironments/Assets/Scripts/SensorsKit.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SensorsKit : MonoBehaviour {
-    // Initialization code
     private void Start() {
+        mTransformComputer =
+                new SensorPropertiesComputer(
+                        mCarTransform,
+                        SENSORS_OFFSET_Y,
+                        SENSORS_OFFSET_Z);
         InitSensorList();
-        // TODO -> czy trzeba tu coś jeszcze dopisać?
     }
     private void InitSensorList() {
         const float ANGLE_BETWEEN_SENSORS = FIELD_OF_VIEW / (RAYS_COUNT - 1);
@@ -18,66 +21,11 @@ public class SensorsKit : MonoBehaviour {
     }
     private CarSensor CreateNewSensor(float aCurrentSensorAngle) {
         var newSensor = new CarSensor();
-        Vector3 sensorOrigin = ComputeSensorOrigin();
-        Vector3 sensorDirection = ComputeSensorDirection(aCurrentSensorAngle);
+        Vector3 sensorOrigin = mTransformComputer.ComputeSensorOrigin();
+        Vector3 sensorDirection =
+                mTransformComputer.ComputeSensorDirection(aCurrentSensorAngle);
         newSensor.SetRayProperties(sensorOrigin, sensorDirection);
         return newSensor;
-    }
-    private Vector3 ComputeSensorOrigin() {
-        var carPosition = mCarTransform.position;
-        Vector3 sensorOriginBeforeRotation =
-                new Vector3(
-                    carPosition.x,
-                    carPosition.y + SENSORS_ORIGIN_Y,
-                    carPosition.z + SENSORS_ORIGIN_Z);
-        float carRotationAngle = mCarTransform.eulerAngles.y;
-        
-        Vector3 sensorOriginAfterRotation =
-                ComputePointRotation(
-                        sensorOriginBeforeRotation,
-                        carPosition,
-                        carRotationAngle);
-        return sensorOriginAfterRotation;
-    }
-    private Vector3 ComputeSensorDirection(float aCurrentSensorAngle) {
-        var carPosition = mCarTransform.position;
-        Vector3 sensorOrigin =
-                new Vector3(
-                        carPosition.x,
-                        carPosition.y + SENSORS_ORIGIN_Y,
-                        carPosition.z + SENSORS_ORIGIN_Z);
-        Vector3 sensorDirectionBeforeRotation =
-                new Vector3(
-                        sensorOrigin.x - 0.5f,
-                        sensorOrigin.y,
-                        sensorOrigin.z);
-        Vector3 directionRotatedBySensorAngle = ComputePointRotation(
-                sensorDirectionBeforeRotation,
-                sensorOrigin,
-                aCurrentSensorAngle);
-        float carRotationAngle = mCarTransform.eulerAngles.y;
-        Vector3 directionRotatedByCarAngle = ComputePointRotation(
-                directionRotatedBySensorAngle,
-                carPosition,
-                carRotationAngle);
-        return directionRotatedByCarAngle;
-    }
-    private Vector3 ComputePointRotation(
-            Vector3 pointToRotate,
-            Vector3 rotationOrigin,
-            float angleInDegrees) {
-        float angleInRadians = angleInDegrees * Mathf.PI / 180.0f;
-        float rotatedX =
-                Mathf.Cos(angleInRadians) * (pointToRotate.x - rotationOrigin.x)
-                - Mathf.Sin(angleInRadians) * (pointToRotate.z - rotationOrigin.z)
-                + rotationOrigin.x;
-        float rotatedZ =
-                Mathf.Sin(angleInRadians) * (pointToRotate.x - rotationOrigin.x)
-                + Mathf.Cos(angleInRadians) * (pointToRotate.z - rotationOrigin.z)
-                + rotationOrigin.z;
-        
-        Vector3 rotatedPoint = new Vector3(rotatedX, pointToRotate.y, rotatedZ);
-        return rotatedPoint;
     }
 
     private List<double> GetDistanceList() {
@@ -89,9 +37,11 @@ public class SensorsKit : MonoBehaviour {
     }
 
     public Transform mCarTransform;
-    public float SENSORS_ORIGIN_Y = 0.03f;
-    public float SENSORS_ORIGIN_Z = 0.026f;
+    public float SENSORS_OFFSET_Y = 0.03f;
+    public float SENSORS_OFFSET_Z = 0.026f;
+
     private const float FIELD_OF_VIEW = 180.0f;
     private const uint RAYS_COUNT = 3;
     private List<CarSensor> mSensorList;
+    private SensorPropertiesComputer mTransformComputer;
 }
