590e587fd70f69592877baa885ddcb535d9c485c
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-08-10 15:17:59 +0200

    Dodanie do projektu pakietu narzędziowego 'Unity ML-Agents'

---
+++ application/unity_mla_environment/RacingEnvironments/Assets/MyScripts/RaceTrackEnv_Scripts/CarCameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCameraFollow : MonoBehaviour
+{
+    public GameObject agentCar;
+    
+    private void Update() {
+          Vector3 pos = agentCar.transform.position;
+          Quaternion rotation = agentCar.transform.rotation;
+          pos.y = 1.0f;
+          transform.position = pos;
+    }
+}
