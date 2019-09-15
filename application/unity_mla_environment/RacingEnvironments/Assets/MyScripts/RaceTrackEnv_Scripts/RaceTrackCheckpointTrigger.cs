7bcc54aaca33f1c425536181fc834220b9e7d7bf
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-09-15 15:01:47 +0200

    Dodanie do środowiska Unity checkpointów zliczających dodatkowe nagrody

---
+++ application/unity_mla_environment/RacingEnvironments/Assets/MyScripts/RaceTrackEnv_Scripts/RaceTrackCheckpointTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceTrackCheckpointTrigger : MonoBehaviour {
+    private const float REWARD_FOR_CHECKPOINT = 1.0f;
+    private HashSet<int> mAgentKeySet;
+
+    void Start() {
+        mAgentKeySet = new HashSet<int>();
+    }
+    
+    public void ResetKeySet() {
+        mAgentKeySet.Clear();
+    }
+
+    void OnTriggerEnter(Collider aCarCollider) {
+        var carObject = aCarCollider.gameObject;
+        var carAgentComponent = carObject.GetComponent<CarAgent>();
+        if (carAgentComponent != null) {
+            var agentID = carObject.GetInstanceID();
+            if (mAgentKeySet.Contains(agentID)) {
+                carAgentComponent.AddReward(-REWARD_FOR_CHECKPOINT);
+            } else {
+                mAgentKeySet.Add(agentID);
+                carAgentComponent.AddReward(REWARD_FOR_CHECKPOINT);
+            }
+        }
+    }
+}
