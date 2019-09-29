925c44884601b3ae8c579bcc062ed76e67f2738d
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-09-29 12:51:01 +0200

    Usunięcie zbędnych testów z pliku src/test/test_train.py

--- application/python_external_process/src/test/test_train.py
+++ application/python_external_process/src/test/test_train.py
@@ -78,56 +78,8 @@ class TestTrain(unittest.TestCase):
                 computeAgentDimensions(observationSize, actionSize, configData)
         self.assertEqual(actualAgentDimensions, expectedAgentDimensions)
 
-    @data(1, 3.14, (1, 2, 3), {"wrong" : "type"}, "kanapka", None)
-    def test_findIndexOfBestModel_FitnessListHasWrongType(self, fitnessList):
-        self.assertRaises(
-                ValueError,
-                findIndexOfBestModel,
-                fitnessList)
-    
-    @unpack
-    @data(([0.3, 1.1, 5.9, 3.6], 2), ([-100.13, -2.3, -5.7, -0.9], 3),
-            ([-100.13, 0.5, -5.7, -0.9], 1))
-    def test_findIndexOfBestModel_OK(self, fitnessList, expectedIndex):
-        actualIndex = findIndexOfBestModel(fitnessList)
-        self.assertEqual(actualIndex, expectedIndex)
-
-    @data(1, 3.14, "kanapka", (3, 14), {"wrong" : "type"}, None)
-    def test_areAllAgentsDone_AgentDonesHasWrongType(self, agentDones):
-        self.assertRaises(
-                ValueError,
-                areAllAgentsDone,
-                agentDones)
-    
-    def test_areAllAgentsDone_SomeElementsAreNotBools(self):
-        agentDones = [True, "kanapka", 3.14, False, {"wrong" : "type"}, True]
-        self.assertRaises(
-                ValueError,
-                areAllAgentsDone,
-                agentDones)
-
     @unpack
     @data(([True, True, False, True], False), ([True, True, True, True], True))
-    def test_areAllAgentsDone_False(self, agentDones, expectedResult):
+    def test_areAllAgentsDone(self, agentDones, expectedResult):
         actualResult = areAllAgentsDone(agentDones)
         self.assertEqual(actualResult, expectedResult)
-    
-    @data(1, 3.14, "kanapka", {"wrong" : "type"}, None, (1, 2, 3, 4))
-    def test_getBestFitness_FitnessListHasWrongType(self, fitnessList):
-        self.assertRaises(
-                ValueError,
-                getBestFitness,
-                fitnessList)
-    
-    def test_getBestFitness_FitnessListIsEmpty(self):
-        fitnessList = []
-        self.assertRaises(
-                ValueError,
-                getBestFitness,
-                fitnessList)
-
-    @unpack
-    @data(([-1.3, 2.0, 10.13, -4.1], 10.13), ([-10.9, -2.0, -0.9, -33.3], -0.9))
-    def test_getBestFitness_OK(self, fitnessList, expectedValue):
-        actualValue = getBestFitness(fitnessList)
-        self.assertEqual(actualValue, expectedValue)
