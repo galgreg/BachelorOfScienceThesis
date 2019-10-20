b9a8b5c4fd272b6d2c32d44db18574997f28e200
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-10-20 19:33:09 +0200

    Dodanie brakujących testów po stronie Pythona

--- application/python_external_process/src/test/training/test_training_utilities.py
+++ application/python_external_process/src/test/training/test_training_utilities.py
@@ -1,5 +1,6 @@
 import json
 from os import remove
+from src.AgentNeuralNetwork import *
 from src.training.training_utilities import *
 from ddt import ddt, data, unpack
 import unittest
@@ -43,3 +44,97 @@ class TestTrainingUtilities(unittest.TestCase):
         actualConfigData = loadConfigData(tempTestFilePath)
         self.assertEqual(actualConfigData, expectedConfigData)
         remove(tempTestFilePath)
+    
+    def test_computeNumOfParameters(self):
+        agentDimensions = [3, 5, 2]
+        expectedNumOfParameters = 32
+        actualNumOfParameters = computeNumOfParameters(agentDimensions)
+        self.assertEqual(actualNumOfParameters, expectedNumOfParameters)
+    
+    @unpack
+    @data((1, [5, 2]), (10, [5, 3, 2]), (100, [5, 20, 30, 7]))
+    def test_retrieveParametersFromAgents(self, numberOfAgents, agentsDimensions):
+        agentList = [
+                AgentNeuralNetwork(agentsDimensions, False)
+                for i in range(numberOfAgents)
+        ]
+        actualParameters = retrieveParametersFromAgentList(agentList)
+                
+        expectedParametersType = torch.Tensor
+        actualParametersType = type(actualParameters)
+        self.assertEqual(actualParametersType, expectedParametersType)
+        
+        numberOfParameters = 0
+        for i in range(len(agentsDimensions) - 1):
+            numberOfParameters += agentsDimensions[i] * agentsDimensions[i+1] \
+                    + agentsDimensions[i+1]
+        
+        expectedParametersDimensions = [numberOfAgents, numberOfParameters]
+        actualParametersDimensions = actualParameters.size()
+        self.assertEqual(
+                list(actualParametersDimensions),
+                expectedParametersDimensions)
+        
+        expectedParameters = []
+        for agent in agentList:
+            expectedAgentParameters = []
+            for layer in agent._layers:
+                layerWeights = layer.weight
+                numberOfWeights = layerWeights.numel()
+                weightParameters = \
+                        torch.reshape(layerWeights, (numberOfWeights,))
+                expectedAgentParameters = \
+                        expectedAgentParameters + weightParameters.tolist()
+                expectedAgentParameters = \
+                        expectedAgentParameters + layer.bias.tolist()
+            
+            expectedParameters.append(expectedAgentParameters)
+        
+        expectedParameters = torch.tensor(expectedParameters)
+        self.assertTrue(torch.equal(actualParameters, expectedParameters))
+
+    @data([5, 2], [5, 3, 2], [5, 7, 4, 2])
+    def test_setNewParametersOnAgent(self, agentDimensions):
+        agent = AgentNeuralNetwork(agentDimensions)
+        numberOfParameters = 0
+        for i in range(len(agentDimensions) - 1):
+            numberOfParameters += agentDimensions[i] * agentDimensions[i+1] \
+                    + agentDimensions[i+1]
+        newParameters = torch.randn(numberOfParameters)
+        
+        parametersBeforeSet = self._retrieveParameters(agent)
+        setNewParametersOnAgent(agent, newParameters)
+        parametersAfterSet = self._retrieveParameters(agent)
+        
+        for oldLayerParameters, newLayerParameters \
+                in zip(parametersBeforeSet, parametersAfterSet):
+            # Assert weight and bias parameters
+            for oldParameters, newParameters \
+                    in zip(oldLayerParameters, newLayerParameters):
+                expectedTypeOfParameters = torch.Tensor
+                self.assertEqual(
+                        type(oldParameters),
+                        expectedTypeOfParameters)
+                self.assertEqual(
+                        type(newParameters),
+                        expectedTypeOfParameters)
+                expectedDimensionsOfParameters = \
+                        list(oldParameters.size())
+                actualDimensionsOfParameters = \
+                        list(newParameters.size())
+                self.assertEqual(
+                        actualDimensionsOfParameters,
+                        expectedDimensionsOfParameters)
+                self.assertFalse(
+                        torch.equal(newParameters, oldParameters))
+
+    def _retrieveParameters(self, agent):
+        agentParameters = []
+        for layer in agent._layers:
+            layerParameters = []
+            copyOfWeights = layer.weight.data.clone().detach()
+            layerParameters.append(copyOfWeights)
+            copyOfBiases = layer.bias.data.clone().detach()
+            layerParameters.append(copyOfBiases)
+            agentParameters.append(layerParameters)
+        return agentParameters
